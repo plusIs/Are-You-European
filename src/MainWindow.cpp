4ea66bb9adf05123923e3270a2e88b8e75b05ec7
plusis <plusls@qq.com>
2018-02-04 21:25:18 +0800

    init clion

--- src/MainWindow.cpp
+++ src/MainWindow.cpp
@@ -3,3 +3,61 @@
 //
 
 #include "MainWindow.h"
+#include <QTime>
+
+MainWindow::MainWindow(QMainWindow* parent)
+        : QMainWindow(parent),
+          ui(new Ui::MainWindow) {
+    this->ui->setupUi(this);
+    for (int i = 0; i < 5; ++i) {
+        this->ui->lotteryListWidget->item(i)->setTextAlignment(Qt::AlignHCenter | Qt::AlignVCenter);
+
+    }
+    this->lotteryList = new LotteryList();
+    this->lotteryList->loadFromFile("data.txt");
+
+    //this->ui->circleLabel->setPixmap(QPixmap(":/images/circle.png").scaled(80, 80));
+}
+
+MainWindow::~MainWindow()
+{
+    delete this->ui;
+    delete this->lotteryList;
+}
+
+void MainWindow::on_funPushButton_clicked() {
+    static LotteryList::iterator nowIterator = this->lotteryList->begin();
+    static bool stop = false;
+    LotteryList::iterator tmpIterator;
+    if (this->ui->funPushButton->text() == "Start") {
+        stop  = false;
+        this->ui->funPushButton->setText("Stop");
+        while(stop == false) {
+            tmpIterator = nowIterator;
+            nowIterator = this->lotteryList->nextItreator(nowIterator);
+            for (int i = 0; i < 5; ++i) {
+                this->ui->lotteryListWidget->item(i)->setText(tmpIterator->c_str());
+                tmpIterator = this->lotteryList->nextItreator(tmpIterator);
+            }
+            msleep(1000);
+        }
+
+        this->ui->funPushButton->setEnabled(true);
+        this->ui->funPushButton->setText("Start");
+
+    } else {
+        this->ui->funPushButton->setEnabled(false);
+        stop = true;
+    }
+
+
+
+}
+
+void msleep(unsigned int msecs) {
+    QTime reachTime = QTime::currentTime().addMSecs(msecs);
+    while(QTime::currentTime() < reachTime)
+    {
+        QApplication::processEvents(); //让Qt去处理未处理的事件
+    }
+}
--- src/MainWindow.h
+++ src/MainWindow.h
@@ -4,11 +4,38 @@
 
 #ifndef ARE_YOU_EUROPEAN_MAINWINDOW_H
 #define ARE_YOU_EUROPEAN_MAINWINDOW_H
+#include <QtWidgets/QMainWindow>
+#include <QListWidget>
+
+#include "ui_MainWindow.h"
+
+#include "LotteryList.h"
 
 
-class MainWindow {
+class MainWindow : public QMainWindow{
+    Q_OBJECT
+public:
+    MainWindow(QMainWindow* parent = 0);
+    ~MainWindow();
+
+
+private slots:
+    /*void on_contactListWidget_currentItemChanged(QListWidgetItem *current, QListWidgetItem *previous);
+    void on_searchLineEdit_returnPressed();
+    void on_removePushButton_clicked();
+    void on_editPushButton_clicked();
+    void on_addPushButton_clicked();
+    void on_nameLineEdit_editingFinished();*/
+    void on_funPushButton_clicked();
+
+
+private:
+    Ui::MainWindow *ui;
+    LotteryList *lotteryList;
+    //bool edit = false;
 
 };
 
+void msleep(unsigned int msecs);
 
 #endif //ARE_YOU_EUROPEAN_MAINWINDOW_H
